09290af310b8257cb079fa1e728877eb563792c4
TFluke <fazetfluke@gmail.com>
2026-06-14 11:46:00 -0400

    Revamped and made a lot of changes

--- app/static/js/fade.js
+++ app/static/js/fade.js
@@ -1,20 +1,103 @@
 const DELAY = 700;
+const MAX_HIGHLIGHT = 4;
+const MIN_HIGHLIGHT = 0;
+
+let current_main = 2;
+
+
+//TODO: Remove the animation and make it transitions.
+//      Use CSS variables to shift each frame by the offset + their width
 
 async function coroutine(delay) {
     await new Promise(r => setTimeout(r, delay));
 }
 
+function get_id(num) {
+    const text = "highlight-";
+    return text.concat("", num.toString()); 
+}
+
+function transition_left() {
+
+    current_main -= 1;
+
+    // document.getElementById(get_id(current_main - 1)).classList.remove("outer-left-frame");
+    document.getElementById(get_id(current_main - 1)).classList.add("shfit-right");
+    document.getElementById(get_id(current_main - 1)).addEventListener("click", transition_left);
+
+    document.getElementById(get_id(current_main)).removeEventListener("click", transition_left);
+    document.getElementById(get_id(current_main)).classList.remove("left-frame");
+    if (document.getElementById(get_id(current_main)).classList.contains("left-start")) {
+        document.getElementById(get_id(current_main)).classList.remove("left-start");
+    }
+    document.getElementById(get_id(current_main)).classList.add("main-frame");
+
+    document.getElementById(get_id(current_main + 1)).classList.remove("main-frame");
+    document.getElementById(get_id(current_main + 1)).classList.add("right-frame");
+    document.getElementById(get_id(current_main + 1)).addEventListener("click", transition_right);
+
+    document.getElementById(get_id(current_main + 2)).removeEventListener("click", transition_right);
+    document.getElementById(get_id(current_main + 2)).classList.remove("right-frame");
+    if (document.getElementById(get_id(current_main)).classList.contains("right-start")) {
+        document.getElementById(get_id(current_main)).classList.remove("right-start");
+    }
+    document.getElementById(get_id(current_main + 2)).classList.add("out-right-frame");
+
+}
+
+function transition_right() {
+
+    current_main += 1;
+
+    document.getElementById(get_id(current_main + 1)).classList.add("shift_left");
+    document.getElementById(get_id(current_main + 1)).addEventListener("click", transition_right);
+
+    document.getElementById(get_id(current_main)).removeEventListener("click", transition_right);
+    document.getElementById(get_id(current_main)).classList.remove("shift_left");
+    if (document.getElementById(get_id(current_main)).classList.contains("right_start")) {
+        document.getElementById(get_id(current_main)).classList.remove("right_start");
+    }
+    document.getElementById(get_id(current_main)).classList.add("main-frame");
+
+    document.getElementById(get_id(current_main - 1)).classList.remove("main-frame");
+    document.getElementById(get_id(current_main - 1)).classList.add("shift_left");
+    document.getElementById(get_id(current_main - 1)).addEventListener("click", transition_left);
+
+    document.getElementById(get_id(current_main - 2)).removeEventListener("click", transition_left);
+    document.getElementById(get_id(current_main - 2)).classList.remove("left-frame");
+    if (document.getElementById(get_id(current_main)).classList.contains("left-start")) {
+        document.getElementById(get_id(current_main)).classList.remove("left-start");
+    }
+    document.getElementById(get_id(current_main - 2)).classList.add("out-left-frame");
+
+    console.log("Check point");
+
+}
+
 window.onload = async () => {
+
+    document.querySelector(".left-start").addEventListener("click", transition_left);
+
+    document.querySelector(".right-start").addEventListener("click", transition_right);
+    
+    for (let i = MIN_HIGHLIGHT; i < MAX_HIGHLIGHT + 1; i++) {
+        const div = document.getElementById(get_id(i));
+        div.addEventListener("animationend", () => {
+            if (div.classList.contains("shift-left")) {
+                div.classList.remove("shift-left");
+            }
+
+            if (div.classList.contains("shift-right")) {
+                div.classList.remove("shift-right");
+            }
+        });
+    }
+
     await coroutine(DELAY);
-    document.getElementById("hero-text-where").classList.add('show');
+    document.getElementById("welcome-header").classList.add('show');
     await coroutine(DELAY);
-    document.getElementById("hero-text-ai").classList.add('show');
-    await coroutine(DELAY);
-    document.getElementById("hero-text-meets").classList.add('show');
-    await coroutine(DELAY);
-    document.getElementById("hero-text-consulting").classList.add('show');
-    await coroutine(DELAY);
-    document.getElementById("hero-text-paragraph").classList.add('show');
+    document.getElementById("pioneers-header").classList.add('show');
+
 
 };
 
@@ -24,7 +107,7 @@ const elements = document.getElementById("main-meet-team-div").querySelectorAll(
 const observer = new IntersectionObserver(entires => {
     entires.forEach(entry => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('show-transition');
         }
     });
 });
