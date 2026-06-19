f3472cb68596176f312f949e3011e846f12c22eb
TFluke <fazetfluke@gmail.com>
2026-06-19 17:27:26 -0400

    Made the files for the github pages

--- fade.js
+++ fade.js
@@ -1,38 +1,21 @@
-const DELAY = 700;
-
-async function coroutine(delay) {
-    await new Promise(r => setTimeout(r, delay));
-}
 
 window.onload = async () => {
-    await coroutine(DELAY);
-    document.getElementById("hero-text-where").classList.add('show');
-    await coroutine(DELAY);
-    document.getElementById("hero-text-ai").classList.add('show');
-    await coroutine(DELAY);
-    document.getElementById("hero-text-meets").classList.add('show');
-    await coroutine(DELAY);
-    document.getElementById("hero-text-consulting").classList.add('show');
-    await coroutine(DELAY);
-    document.getElementById("hero-text-paragraph").classList.add('show');
+
+    // New operations
+
+    const jiya_div = document.getElementById("team-div-jiya");
+    const jiya_hover_panel = document.getElementById("info-panel-jiya");
+    const jiya_hover_div = document.getElementById("info-div-jiya");
+    jiya_hover_panel.addEventListener("mouseenter", () => {
+        jiya_hover_div.classList.remove("hide-transition");
+        jiya_hover_div.classList.add("show-transition");
+    })
+
+    jiya_div.addEventListener("mouseleave", () => {
+        jiya_hover_div.classList.remove("show-transition");
+        jiya_hover_div.classList.add("hide-transition");
+    })
 
 };
 
-// Selects all elements that have the class = fade in from the webpage
-const elements = document.getElementById("main-meet-team-div").querySelectorAll('.hidden-transition');
 
-const observer = new IntersectionObserver(entires => {
-    entires.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-});
-
-
-elements.forEach(el => {
-  observer.observe(el);
-  if (el.getBoundingClientRect().top < window.innerHeight) {
-    el.classList.add('show');
-  }
-});
